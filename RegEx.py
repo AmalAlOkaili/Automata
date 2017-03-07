9a6e3f21550f71c438d346ade47c0638d86615d0
Amal Al-Okaili <amalokaili@gmail.com>
2017-03-07 16:22:18 +0300

    Add files via upload

---
+++ RegEx.py
@@ -0,0 +1,59 @@
+import re
+
+def find_consonant(word): #Checks TWO consecutive consonants
+    for match in re.findall(r"\w+[^ouieaOUIEA]{2}\b", word):
+          print match
+
+
+def find_vowel(word):  #Checks FIRST and LAST chars
+        for match in re.findall("\w{5,}", word):
+            if re.findall(r"(^[ouieaOUIEA].+[ouieaOUIEA]$)", match):
+                 print match
+
+
+def find_consonant_vowel(word):    #Checks FIRST and LAST chars {consonant , vowel}
+     for match in re.findall("\w{7,}", word):
+         if re.findall(r"(^[^ouieaOUIEA].+[ouieaOUIEA]$)", match):
+          print match
+
+
+def find_after_vowel(word): #Checks letter after VOWELS
+    for match in re.findall(r"[ouieaOUIEA](\w+)", word):
+          print match
+
+#def find_abbrv(word):
+
+def eXtract(word):
+    matches = re.finditer('.+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)', word)
+    print [m.group(0) for m in matches]
+
+
+
+DATA = []
+input_DATA = raw_input("Enter the data\n")
+
+find_consonant(input_DATA)
+find_vowel(input_DATA)
+find_consonant_vowel(input_DATA)
+find_after_vowel(input_DATA)
+
+
+'''
+re.sub
+Can be used to abbreviate King Saud University to KSU
+'''
+'''
+find_consonant("Returning to physics")
+
+print("\n************\n")
+
+find_vowel("Thirty years agooooo")
+
+print("\n************\n")
+
+find_consonant_vowel("Fameeeeee is fleeting")
+
+print("\n************\n")
+
+find_after_vowel("Take it sleazy - Adam.")
+'''
